72417031ef8a4b6eac760ddf9e3cc021de74460d
Nicolas Marini <nmarini44@gmail.com>
2018-07-01 19:45:38 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
   end
 
   def valid?
-    if @side_1 + @side_2 =< @side_3 || @side_3 + @side_1 =< @side_2 || @side_3 + @side_2 =< @side_1 || self.sides.any?{|side| side == 0}
+    if @side_1 + @side_2 <= @side_3 || @side_3 + @side_1 <= @side_2 || @side_3 + @side_2 <= @side_1 || self.sides.any?{|side| side == 0}
     false
     else
     true
