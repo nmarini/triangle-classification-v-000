c45198d02189e1230e2b388b121099d19a2ee659
Nicolas Marini <nmarini44@gmail.com>
2018-07-01 19:37:53 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,10 +1,12 @@
 class Triangle
-  attr_accessor :side_1, :side_2, :side_3
+  attr_accessor :side_1, :side_2, :side_3, :sides
 
   def initialize(side_1, side_2, side_3)
-    @side_1 = side_1
-    @side_2 = side_2
-    @side_3 = side_3
+    @sides = [
+    @side_1 = side_1,
+    @side_2 = side_2,
+    @side_3 = side_3,
+    ]
   end
 
   def valid?
